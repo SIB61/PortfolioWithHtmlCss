26042c9791c97eb29c2a894bfb08d046b7c636a8
SIB61 <sib.sustswe@gmail.com>
2022-04-14 01:42:11 +0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var typed_js_1 = require("typed.js");
+var typed = new typed_js_1.Typed(".autotype", {
+    strings: ["Fullstack", "Android and Web", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: true
+});
